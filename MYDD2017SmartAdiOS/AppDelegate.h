cb54f36a851661be275c47b16b08433eb437ffd1
Seyedvahid Dianat <Vahid@Yasamans-MacBook-Pro.local>
2017-08-11 20:56:52 +0800

    Initial Commit

---
+++ MYDD2017SmartAdiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYDD2017SmartAdiOS
+//
+//  Created by Seyedvahid Dianat on 11/08/2017.
+//  Copyright © 2017 Seyedvahid Dianat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYDD2017SmartAdiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYDD2017SmartAdiOS
+//
+//  Created by Seyedvahid Dianat on 11/08/2017.
+//  Copyright © 2017 Seyedvahid Dianat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
