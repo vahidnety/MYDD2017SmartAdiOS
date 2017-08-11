c89a0ae4d02925e642166da286517b16ba62a700
Seyedvahid Dianat <Vahid@Yasamans-MacBook-Pro.local>
2017-08-11 22:14:16 +0800

    map configuration

--- MYDD2017SmartAdiOS/AppDelegate.m
+++ MYDD2017SmartAdiOS/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+@import GoogleMaps;
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [GMSServices provideAPIKey:@"AIzaSyD3bG1iIUrLGNSH7HJWoPIShL-b-8nBPVA"];
+
     return YES;
 }
 
